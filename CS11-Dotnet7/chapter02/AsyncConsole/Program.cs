b433a6d9fc53d82a346fcdc1e8126ab5c87903bd
Walid Bouladam <bouladam.walid86@gmail.com>
2023-02-04 11:44:55 +1300

    Completed Chapter 2: AsyncConsole section

---
+++ CS11-Dotnet7/chapter02/AsyncConsole/Program.cs
@@ -0,0 +1,3 @@
+﻿HttpClient client = new();
+HttpResponseMessage response = await client.GetAsync("http://www.microsoft.com");
+WriteLine("Microsoft's home page has {0:N0} bytes.", response.Content.Headers.ContentLength);
