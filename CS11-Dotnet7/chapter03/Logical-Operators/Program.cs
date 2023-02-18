fd54f38967fc7f18b661fa1e1f6c4eb0ec28c395
Walid Bouladam <bouladam.walid86@gmail.com>
2023-02-18 18:56:45 +1300

    Completed Chapter 3: Conditional Operators

---
+++ CS11-Dotnet7/chapter03/Logical-Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿bool a = true;
+bool b = false;
+WriteLine($"AND  | a     | b    ");
+WriteLine($"a    | {a & a,-5} | {a & b,-5} ");
+WriteLine($"b    | {b & a,-5} | {b & b,-5} ");
+WriteLine();
+WriteLine($"OR   | a     | b    ");
+WriteLine($"a    | {a | a,-5} | {a | b,-5} ");
+WriteLine($"b    | {b | a,-5} | {b | b,-5} ");
+WriteLine();
+WriteLine($"XOR  | a     | b    ");
+WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5} ");
+WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5} ");
+
+WriteLine();
+WriteLine($"a & DoStuff() = {a & DoStuff()}");
+WriteLine(); 
+WriteLine($"b & DoStuff() = {b & DoStuff()}");
+
+WriteLine();
+WriteLine($"a && DoStuff() = {a && DoStuff()}");
+WriteLine(); 
+WriteLine($"b && DoStuff() = {b && DoStuff()}");
+
+// Conditional logical operators:
+
+static bool DoStuff()
+{
+    WriteLine("Doing stuff...");
+    WriteLine();
+    return true;
+}
